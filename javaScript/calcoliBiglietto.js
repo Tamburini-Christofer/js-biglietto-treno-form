fb1bf86095c6625c70acb7b562ac743a02dbcd1a
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-05 19:01:42 +0200

    Creazione input + lavoro concluso

--- javaScript/calcoliBiglietto.js
+++ javaScript/calcoliBiglietto.js
@@ -12,92 +12,78 @@ function calcoloSconto (valueKm, valueAge) {
 
 //Creiamo una variabile che conterrà il prezzo finale
     let prezzoFinale = 0;
+    let tipoBiglietto = "";
 
 //Creiamo un ciclo if per calcolare quale scelta del burger menu
     if (option === 1) {
-        prezzoFinale = sumEntire * 0.8;
+      prezzoFinale = sumEntire * 0.8;
+      tipoBiglietto = "Minorenne";
     } else if (option === 2) {
        prezzoFinale = sumEntire;
+       tipoBiglietto = "Standard";
     } else if (option === 3){
         prezzoFinale = sumEntire * 0.6;
+        tipoBiglietto = "Over-65";
     }
-    return prezzoFinale.toFixed(2);
+    return {
+        prezzo: prezzoFinale.toFixed(2),
+        tipo: tipoBiglietto
+    };
+
 }
+
+/******************************************************************************************** */
 
 //Ora selezioniamo gli elementi di uotput
 //Nome del passeggero
-const nomePasseggero = document.getElementById("nomePasseggero");
+const inputPasseggero = document.getElementById("nomePasseggero");
 //Km da percorrere
-const kmPercorso = document.getElementById ("km");
+const inputKm = document.getElementById ("km");
 //età passeggero
-const etaPasseggero = document.getElementById ("eta");
+const inputEta = document.getElementById ("eta");
 
 //Ora selezioniamo il form per richiamare l'evento
 const bigliettoForm = document.querySelector ("#formBiglietto");
-
 //Selezioniamo bottone 
 const bottoneForm = document.getElementById("generaBiglietto");
 //Selezioniamo bottone reset
-const cottoneReset = document.getElementById("reset");
-
+const bottoneReset = document.getElementById("reset");
 /************************************************************* */
+
 //Nome Output biglietto 
 //Nome biglietto
-const nomeBiglietto = document.getElementById("nomeBiglietto");
+const outPasseggero = document.getElementById("nome_passeggero");
 //Tipo biglietto
-const tipoBiglietto = document.getElementById("tipoBiglietto");
+const outTipoBiglietto = document.getElementById("tipoBiglietto");
 //Carrozza
-const carrozza = document.getElementById("carozza");
+const outCarrozza = document.getElementById("carrozza");
 //Codice Cp
 const CodCpBiglietto = document.getElementById("codiceCP");
 //Costo finale biglietto stampato
-const costoBiglietto = document.getElementById("costo_biglietto");
+const outCostoBiglietto = document.getElementById("costo_biglietto");
 
 /*****************************************************************/
 /*Submittamo*/
 bigliettoForm.addEventListener("submit", (evento) => {
+//Blocchiamo la trasmissione automatica 
         evento.preventDefault();
-        
-        let prezzoFinale = calcoloSconto(parseFloat(kmPercorso.value), etaPasseggero.value);
-        nomePasseggero.innerHTML = nomeBiglietto.value;
-        costoBiglietto.innerHTML = prezzoFinale + " €";
+//Assegniamo una variabile alla funzione  
+        let prezzoBiglietto = calcoloSconto(parseFloat(inputKm.value), inputEta);
+        //Scriviamo gli output
+        outPasseggero.innerHTML = inputPasseggero.value;
+        outTipoBiglietto.innerHTML = prezzoBiglietto.tipo;
+        outCarrozza.innerHTML = Math.floor(Math.random() * 8);
+        CodCpBiglietto.innerHTML = Math.floor(Math.random() * 70000);
+        outCostoBiglietto.innerHTML = prezzoBiglietto.prezzo + " Euro";
 });
+//Scriviamo Il Reset bottone
+bottoneReset.addEventListener("click", () => {
+        bigliettoForm.reset();
+        outPasseggero.innerHTML = "";
+        outTipoBiglietto.innerHTML = "";
+        outCarrozza.innerHTML = "";
+        CodCpBiglietto.innerHTML = "";
+        outCostoBiglietto.innerHTML = "";
+}); 
 
 
-// // Comunicare al cliente il valore euro/km
-// console.log(`il costo del biglietto è di ${ticketCost} €/km`);
-
-// // //Il programma chiede all'utente il numero di km e l'età
-// // const km = prompt("Quanti km vuoi percorrere?");
-// // const age = prompt("Quanti anni hai?");
-// // console.log(`Hai confermato di voler percorrere ${km}km e di avere ${age} anni`);
-
-// // //Calcolo lo sconto del 20% sui minorenni 
-//     if(age<=17) {
-//         console.log("In quanto minorenne hai accesso allo sconto Minorenni!!!")
-
-//     } else if (age>=65) {
-//         console.log("In quanto Over 60 hai accesso allo sconto Over 65!!!"
-//         )
-//     }
-
-// // // Calcolo sconto
-// // const sumDiscChild= (ticketCost * km) / 100 * discountChild;
-// // const sumDiscOld= (ticketCost * km) / 100 * discountOld ;
-// // if (age <= 17) {
-// //     console.log(`Lo sconto è di: ${sumDiscChild.toFixed(2)} €`);
-// // }  else if (age >= 65) {
-// //     console.log(`Lo sconto è di: ${sumDiscOld.toFixed(2)} €`);
-// // }
-
-// // Calcolo totale da pgare
-// // const sumEntire= (ticketCost * km); 
-// // const sumChild = (ticketCost * km) - sumDiscChild;
-// // const sumOld = (ticketCost * km) - sumDiscOld;
-// // if (age <= 17) {
-// //     console.log(`Il totale da pagare è di ${sumChild.toFixed(2)} €`);
-// // } else if ((age >= 18) && (age <64)) {
-// //     console.log(`Il totale da pagare è di ${sumEntire.toFixed(2)} €`);
-// // }  else {
-// //     console.log(`Il totale da pagare è di ${sumOld.toFixed(2)} €`);
-// // }
